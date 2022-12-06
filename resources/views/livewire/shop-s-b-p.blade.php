7ae314c71f1cef27d0cb252bb60dc39d52d35a2a
devhemt <96639094+devhemt@users.noreply.github.com>
2022-12-06 11:19:34 +0700

    delete something

--- resources/views/livewire/shop-s-b-p.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <div class="price-range">
-        <div id="slider-range" wire:ignore></div>
-        <span>Price :</span>
-        <input type="text" id="amount">
-    </div>
-</div>
--- resources/views/livewire/sortlist.blade.php
+++
@@ -1,9 +0,0 @@
-<select wire:model="status" class="orderby" name="orderby">
-    @foreach ($options as $op)
-        @if ($op=='default sort')
-            <option selected="selected" value="{{ $op }}">{{ $op }}</option>
-        @else
-        <option value="{{ $op }}">{{ $op }}</option>
-        @endif
-    @endforeach
-</select>
